88979595f32fed596137b17915893ddf61282bb5
mjnaveen96 <31090107+mjnaveen96@users.noreply.github.com>
2018-12-05 17:48:54 +0530

    ghjh

--- app/tree/Directives/empIdDirective.js
+++ app/tree/Directives/empIdDirective.js
@@ -2,9 +2,29 @@
   angular
   .module('magicTree')
   .directive('empIdDirective',empIdDirectiveFun);
-  function empIdDirectiveFun() {
+  empIdDirectiveFun.$inject = ['$http'];
+  function empIdDirectiveFun($http) {
     return {
+      link: link,
       templateUrl:"app/tree/Templates/empIdTemplate.html"
+    }
+    function link(scope,element,attrs) {
+      // scope.empId='';
+      // console.log(scope.empId);
+      $http({
+        method: 'GET',
+        url:'newdata.json'
+      }).then(function(response) {
+        // console.log(response.data);
+        scope.datArr=[];
+        for(i=0;i<10;i++){
+          scope.datArr.push(response.data[i].id);
+            if(scope.datArr.push(response.data[i].id)===scope.empId){
+          console.log(scope.datArr[i]);
+          }
+
+        }
+      })
     }
   }
 })();
